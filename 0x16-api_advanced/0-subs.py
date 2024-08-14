f9ea443d6b14cbb205f370089d2501a476757139
PaulOnyejela <paulonyejela@gmail.com>
2024-08-14 06:09:47 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 """
-0-main
+This script queries the Reddit API and returns the number of total subscribers for a given subreddit.
 """
+import requests
 import sys
 
-if __name__ == '__main__':
-    from 0_subs import number_of_subscribers
+
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+
+    :param subreddit: The subreddit to query.
+    :return: The number of subscribers, or 0 if an error occurs.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Python/1.0 (Holberton School 0x16 task 0)'}
     
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an error for HTTP codes 4xx/5xx
+        data = response.json().get('data', {})
+        return data.get('subscribers', 0)
+    except (requests.RequestException, ValueError):
+        return 0
+
+
+if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Error: Please provide a subreddit name as an argument.")
+        print("Please pass an argument for the subreddit to search.")
     else:
-        subscribers_count = number_of_subscribers(sys.argv[1])
-        print(f"{subscribers_count}")
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
